b4b43bd6e31d810bf5e08bc5015ff0c9b1f81883
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-22 11:55:42 +0100

    Add flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,5 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |airport| [ airport.code, airport.id ] }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :flights, only: [:index]
-  
+  resources :flights, only: [ :index ]
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "flights#index"
