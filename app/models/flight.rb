b1f622299bcc704101332c351389c192415920e4
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-22 10:58:31 +0100

    Add flights index route and controller

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,5 +3,5 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
 
   validates :start_datetime, presence: true
-  validates :duration, presence: true, numericality: {greater_than: 0}
+  validates :duration, presence: true, numericality: { greater_than: 0 }
 end
