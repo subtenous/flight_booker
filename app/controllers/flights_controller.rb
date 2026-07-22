ed5b4c9aa0c0736d4a8769f52a1b3a7324b38bab
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-22 12:42:56 +0100

    Add flight search filtering

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,17 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |airport| [ airport.code, airport.id ] }
+
+    if params[:departure_airport_id].present?
+      @flights = Flight.where(departure_airport_id: params[:departure_airport_id], arrival_airport_id: params[:arrival_airport_id])
+
+      selected_date = Date.parse(params[:date])
+
+      @flights = Flight.where(start_datetime: selected_date.all_day)
+    end
+
+
+
+
   end
 end
