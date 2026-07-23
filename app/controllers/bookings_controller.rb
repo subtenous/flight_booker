88c3b8c9774dc3a7dbc564c1d7f16666126f5881
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-23 14:58:45 +0100

    Add booking form with nested passenger fields

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,10 @@
 class BookingsController < ApplicationController
   def new 
-    
+    @flight = Flight.find(params[:flight_id])
+    @booking = @flight.bookings.build 
+
+    params[:passengers].to_i.times do
+      @booking.passengers.build 
+    end
   end
 end
