b1cafc844e8f2604b258a81ebfe69716a7b47a65
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-24 14:58:34 +0100

    Create bookings and display booking confimation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def show 
+    @booking = Booking.find(params[:id])
+  end
+  
   def new 
     @flight = Flight.find(params[:flight_id])
     @booking = @flight.bookings.build 
@@ -6,5 +10,22 @@ class BookingsController < ApplicationController
     params[:passengers].to_i.times do
       @booking.passengers.build 
     end
+  end
+
+  def create 
+    @booking = Booking.new(booking_params)
+
+    if @booking.save 
+      redirect_to @booking
+    else
+      @flight = @booking.flight
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.expect(booking: [:flight_id, passengers_attributes: [[:name, :email]]])
   end
 end
