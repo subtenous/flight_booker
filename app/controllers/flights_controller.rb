466c7964eaf0c873c86ce29b23c916c4c10e82cb
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-26 20:00:58 +0100

    Limit flight search to available dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,12 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |airport| [ airport.code, airport.id ] }
 
+    available_dates = Flight.order(:start_datetime).pluck(:start_datetime).map { |datetime| datetime.to_date }.uniq
+
+    @date_options = available_dates.map do |date|
+      [date.strftime("%A, %d %B %Y"), date]
+    end
+
     if params[:departure_airport_id].present?
       @flights = Flight.where(departure_airport_id: params[:departure_airport_id], arrival_airport_id: params[:arrival_airport_id])
 
