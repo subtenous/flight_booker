fb053a56168e924f3eb1af5e3f0a0545e2fe0d3c
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-29 17:15:48 +0100

    Validation change to show less errors

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,9 @@
 class Passenger < ApplicationRecord
   belongs_to :booking
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
+  validates :name, length: { minimum: 2, maximum: 50 }, allow_blank: true
+
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
 end
